dec5430ec9e5862db9f3500b924260b1c6a4f1c6
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-05-09 17:45:03 +0200

    Hopefully a stable bundle

--- install.sh
+++ install.sh
@@ -2,9 +2,14 @@
 
 git submodule update --init --recursive
 
+function fatal_error() {
+    echo "Something went wrong, aborting"
+    exit 1
+}
+
 FOLDERS="aw-core aw-client aw-server aw-watcher-afk aw-watcher-x11"
 for FOLDER in $FOLDERS; do
     cd $FOLDER
-    sudo python3 setup.py develop
+    sudo python3 setup.py develop || fatal_error
     cd ..
 done
