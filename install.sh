30cd68f638b6a19696bb3f0e0c4ae5774c7621a4
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-05-25 14:27:00 +0200

    Added install as user to install script

--- install.sh
+++ install.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-git submodule update --init --recursive
+#git submodule update --init --recursive
 
 function fatal_error() {
     echo "Something went wrong, aborting"
     exit 1
 }
 
+function install_package() {
+    if [[ $USER_INSTALL ]]; then
+        python3 setup.py develop --user || fatal_error
+    else
+        sudo python3 setup.py develop || fatal_error
+    fi
+}
+
+if [[ $1 == '--user' ]]; then
+    USER_INSTALL=true
+    echo "Installing without root"
+fi
+
 FOLDERS="aw-core aw-client aw-server aw-watcher-afk aw-watcher-x11"
 for FOLDER in $FOLDERS; do
     cd $FOLDER
-    sudo python3 setup.py develop || fatal_error
+    install_package
     cd ..
 done
