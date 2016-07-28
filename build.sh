b3e0ce4afffc5448ccc3e22bc550ded14b054306
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-07-28 11:36:27 +0200

    Updated build script, now exits with non-zero exit code if build fails

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ for target in $PYINSTALL_TARGETS; do
     echo
     echo "-------------------------------------"
     echo "Building $target_dir"
-    pyinstaller $target --onefile --clean
+    pyinstaller $target --onefile --clean || echo "Failed to build $target" && exit 1
 done
 
 echo
