a830fa8b10c0de544e99f0a7aadd12c39943a7cf
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-07-28 11:32:53 +0200

    Now builds all pyinstaller specs, nightly added as allowed failure, and submodule updates!

--- build.sh
+++ build.sh
@@ -6,6 +6,24 @@
 
 PYINSTALL_TARGETS=$(find -maxdepth 2 | egrep 'aw-.*/.*\.spec')
 
+echo "PyInstaller .spec files found"
 for target in $PYINSTALL_TARGETS; do
-    pyinstaller $target
+    echo " - $target";
 done
+rm -rf dist build
+
+for target in $PYINSTALL_TARGETS; do
+    target_dir=$(dirname $target)
+
+    echo
+    echo "-------------------------------------"
+    echo "Building $target_dir"
+    pyinstaller $target --onefile --clean
+done
+
+echo
+echo "-------------------------------------"
+echo "Contents of ./dist"
+ls -l dist
+echo "-------------------------------------"
+
