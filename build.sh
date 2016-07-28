0805a9d27ec0cb4625c4de5def1a1350897a0228
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-07-28 11:42:16 +0200

    Fixed error in build script

--- build.sh
+++ build.sh
@@ -6,6 +6,11 @@
 
 PYINSTALL_TARGETS=$(find -maxdepth 2 | egrep 'aw-.*/.*\.spec')
 
+function build_fail {
+    echo "Failed to build $1"
+    exit 1
+}
+
 echo "PyInstaller .spec files found"
 for target in $PYINSTALL_TARGETS; do
     echo " - $target";
@@ -18,7 +23,7 @@ for target in $PYINSTALL_TARGETS; do
     echo
     echo "-------------------------------------"
     echo "Building $target_dir"
-    pyinstaller $target --onefile --clean || echo "Failed to build $target" && exit 1
+    pyinstaller $target --onefile --clean || build_fail
 done
 
 echo
