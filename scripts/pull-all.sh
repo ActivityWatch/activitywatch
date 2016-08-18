0084ec4ab156f24ed8c09c2e54f8c407d3b7279b
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-08-18 17:51:14 +0200

    Added fix to pull-all.sh

--- scripts/pull-all.sh
+++ scripts/pull-all.sh
@@ -4,11 +4,13 @@ git pull
 # Pull other git modules
 git submodule update --init --recursive
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 while getopts "a" opt; do
     case $opt in
         a)
             # Pull bleeding edge from submodules
-            FOLDERS="aw-*"
+            FOLDERS="$DIR/../aw-*"
             for FOLDER in $FOLDERS; do
                 pushd $FOLDER
                 git checkout master
