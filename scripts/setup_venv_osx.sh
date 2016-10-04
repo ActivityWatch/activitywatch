18c389686544dec57d03440c009ab7b23dc8dd79
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-10-04 19:54:18 +0200

    Added such that virtualenv is installed via pip

--- scripts/setup_venv_osx.sh
+++ scripts/setup_venv_osx.sh
@@ -2,6 +2,7 @@
 
 brew update;
 brew install python3;
+pip3 install --upgrade virtualenv;
 virtualenv venv -p python3;
 
 # Now run `source venv/bin/activate` in the shell where the virtualenv should be used
