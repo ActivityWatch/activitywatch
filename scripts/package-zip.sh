e98a1ce09b93380a426e798208d1e238e902664c
Erik Bjäreholt <erik@bjareho.lt>
2017-04-21 13:38:22 +0200

    fixed mistake in last commit

--- scripts/package-zip.sh
+++ scripts/package-zip.sh
@@ -2,32 +2,6 @@
 
 set -e
 
-# Clean
-rm -r dist/ || true
-mkdir dist
-mkdir dist/activitywatch
-
-PYINSTALL_TARGETS=$(find . -maxdepth 2 | egrep 'aw-.*/.*\.spec')
-
-echo "PyInstaller .spec files found"
-for target in $PYINSTALL_TARGETS; do
-    echo " - $target";
-done
-
-for target in $PYINSTALL_TARGETS; do
-    target_dir=$(dirname $target)
-
-    echo
-    echo "-------------------------------------"
-    echo "Building $target_dir"
-    pyinstaller $target --clean
-
-    # Putting it all in one folder
-    cp -r dist/$target_dir/* dist/activitywatch
-    rm -r dist/$target_dir
-done
-
-echo
 echo "-------------------------------------"
 echo "Zipping executables..."
 cd dist;
@@ -35,7 +9,8 @@ platform=$(uname | tr '[:upper:]' '[:lower:]')
 if [[ "$platform" == "darwin" ]]; then
     platform="macos";
 elif [[ $platform == "cygwin"* ]]; then
-    platform="windows"
+    echo "ERROR: cygwin is not a valid platform"
+    exit 1
 fi
 
 if [[ $platform == "windows"* ]]; then
