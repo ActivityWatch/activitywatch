f5cdb1ea3fb308d4de911dceca774489e09331cc
Erik Bjäreholt <erik@bjareho.lt>
2017-04-25 21:13:12 +0200

    [ci/macos] Changed Python installation method, should fix packaging

--- scripts/setup_venv_osx.sh
+++ scripts/setup_venv_osx.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-brew update;
-brew install python3;
-pip3 install --upgrade virtualenv;
+function install_with_brew() {
+    # Will get the latest version in brew.
+    # As of writing, 3.6 and later won't work with PyInstaller so this method is avoided for now.
+    brew update;
+    brew install python3;
+}
+
+function install_with_pkg() {
+    wget -O python3.pkg https://www.python.org/ftp/python/3.5.2/python-3.5.2-macosx10.6.pkg;
+    sudo installer -pkg python3.pkg -target /;
+}
+
+install_with_pkg
+pip3 install --upgrade pip virtualenv;
 virtualenv venv -p python3;
 
 # Now run `source venv/bin/activate` in the shell where the virtualenv should be used
