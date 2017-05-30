2f413d46b70ccc9392b3b461a9887d12efe49524
Erik Bjäreholt <erik@bjareho.lt>
2017-05-30 03:15:16 +0200

    improved the integration test written in Python further

--- scripts/tests/integration_tests.py
+++ scripts/tests/integration_tests.py
@@ -1,24 +1,40 @@
 import subprocess
 from subprocess import PIPE
 from time import sleep
+import sys
+from contextlib import contextmanager
 
 import pytest
 
-server_proc = subprocess.Popen(["aw-server", "--testing"], stdout=PIPE, stderr=PIPE)
 
-# Startup time
-sleep(2)
+# TODO: Write a context manager for the server process
+@contextmanager
+def server_process():
+    server_proc = subprocess.Popen(["aw-server", "--testing"], stdout=PIPE, stderr=PIPE)
+    yield server_proc
+    server_proc.kill()
 
-exit_code = pytest.main(["./aw-server/tests", "-v"])
-print("Tests exit code: " + str(exit_code))
 
-# Cleanup time
-sleep(2)
+def print_section(msg, title="unnamed section"):
+    start_line = "=" * 5 + " " + title + " " + "=" * 5
+    print(start_line)
+    print(msg)
+    print("=" * len(start_line))
 
-server_proc.kill()
+
+with server_process() as server_proc:
+    # Startup time
+    sleep(2)
+
+    exit_code = pytest.main(["./aw-server/tests", "-v"])
+    if exit_code != 0:
+        print("Tests exit code: " + str(exit_code))
+
+    # Cleanup time
+    sleep(2)
+
 out, err = server_proc.communicate()
 
-start_line = "=" * 5 + " aw-server output " + "=" * 5
-print(start_line)
-print(str(err, "utf8"))
-print("=" * len(start_line))
+print_section(str(err, "utf8"), title="aw-server output")
+
+sys.exit(exit_code)
