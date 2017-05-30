beba6c926a421de8d5d5030c6ea25eee75901199
Erik Bjäreholt <erik@bjareho.lt>
2017-05-30 10:15:46 +0200

    improved artifact naming (now includes version)

--- scripts/package/package-zip.sh
+++ scripts/package/package-zip.sh
@@ -5,22 +5,40 @@ set -e
 echo "-------------------------------------"
 echo "Zipping executables..."
 cd dist;
-platform=$(uname | tr '[:upper:]' '[:lower:]')
-if [[ "$platform" == "darwin" ]]; then
-    platform="macos";
-elif [[ $platform == "msys"* ]]; then
-    platform="windows"
-elif [[ $platform == "cygwin"* ]]; then
-    echo "ERROR: cygwin is not a valid platform"
-    exit 1
-fi
 
-echo "Platform is: $platform"
+function get_platform() {
+    _platform=$(uname | tr '[:upper:]' '[:lower:]')
+    if [[ "$platform" == "darwin" ]]; then
+        _platform="macos";
+    elif [[ $platform == "msys"* ]]; then
+        _platform="windows";
+    elif [[ $platform == "cygwin"* ]]; then
+        echo "ERROR: cygwin is not a valid platform";
+        exit 1;
+    fi
+    echo $_platform;
+}
+
+function get_version() {
+    if [[ $TRAVIS_VERSION ]]; then
+        _version=$TRAVIS_VERSION;
+    elif [[ $APPVEYOR_REPO_TAG_NAME ]]; then
+        _version=$APPVEYOR_REPO_TAG_NAME;
+    else
+        _version=$(git rev-parse --short HEAD)
+    fi
+    echo $_version;
+}
+
+platform=$(get_platform)
+version=$(get_version)
+
+echo "Platform: $platform, version: $version"
 
 if [[ $platform == "windows"* ]]; then
-    7z a "activitywatch-${platform}.zip" activitywatch;
+    7z a "activitywatch-${platform}-${version}.zip" activitywatch;
 else
-    zip -r "activitywatch-${platform}.zip" activitywatch;
+    zip -r "activitywatch-${platform}-${version}.zip" activitywatch;
 fi
 cd ..;
 echo "-------------------------------------"
