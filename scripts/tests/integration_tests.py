ad5f802e157aa12ef488bdde1d9453d6b3a825f5
Erik Bjäreholt <erik@bjareho.lt>
2017-05-30 03:23:32 +0200

    improved the integration test

--- scripts/tests/integration_tests.py
+++ scripts/tests/integration_tests.py
@@ -22,19 +22,26 @@ def print_section(msg, title="unnamed section"):
     print("=" * len(start_line))
 
 
-with server_process() as server_proc:
-    # Startup time
-    sleep(2)
+if __name__ == "__main__":
+    with server_process() as server_proc:
+        # Startup time
+        sleep(2)
 
-    exit_code = pytest.main(["./aw-server/tests", "-v"])
-    if exit_code != 0:
-        print("Tests exit code: " + str(exit_code))
+        exit_code = pytest.main(["./aw-server/tests", "-v"])
+        if exit_code != 0:
+            print("Tests exit code: " + str(exit_code))
 
-    # Cleanup time
-    sleep(2)
+        # Cleanup time
+        sleep(2)
 
-out, err = server_proc.communicate()
+    out, err = server_proc.communicate()
+    out, err = (str(stream, "utf8") for stream in (out, err))
+    print_section(err, title="aw-server output")
 
-print_section(str(err, "utf8"), title="aw-server output")
+    error_indicators = ["ERROR"]
+    for s in error_indicators:
+        if s in err:
+            print("Found ERROR indicator in stderr from server: {}".format(s))
+            exit_code = 30 if exit_code == 0 else exit_code
 
-sys.exit(exit_code)
+    sys.exit(exit_code)
