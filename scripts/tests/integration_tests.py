a13146380cd9393e8fff6221ed12892b4afa92b5
Erik Bjäreholt <erik@bjareho.lt>
2017-06-08 18:24:52 +0200

    rewrote integration test setup using pytest

--- scripts/tests/integration_tests.py
+++ scripts/tests/integration_tests.py
@@ -1,47 +1,56 @@
 import subprocess
-from subprocess import PIPE
 from time import sleep
 import sys
 from contextlib import contextmanager
+import tempfile
 
 import pytest
 
 
-# TODO: Write a context manager for the server process
-@contextmanager
+@pytest.fixture(scope="session")
 def server_process():
-    server_proc = subprocess.Popen(["aw-server", "--testing"], stdout=PIPE, stderr=PIPE)
-    yield server_proc
-    server_proc.kill()
+    logfile_stdout = tempfile.NamedTemporaryFile(delete=False)
+    logfile_stderr = tempfile.NamedTemporaryFile(delete=False)
 
+    # logfile_stdout.close()
+    # logfile_stderr.close()
 
-def print_section(msg, title="unnamed section"):
-    start_line = "=" * 5 + " " + title + " " + "=" * 5
-    print(start_line)
-    print(msg)
-    print("=" * len(start_line))
+    @contextmanager
+    def _cm_server_process():
+        server_proc = subprocess.Popen(["aw-server", "--testing"], stdout=logfile_stdout, stderr=logfile_stderr)
+        sleep(2)  # Startup time
+        yield server_proc
+        sleep(2)  # Cleanup time, could probably be removed once tests are synchronous
+        server_proc.kill()
+        server_proc.wait()
+        server_proc.communicate()
 
-
-if __name__ == "__main__":
-    with server_process() as server_proc:
-        # Startup time
-        sleep(2)
-
-        exit_code = pytest.main(["./aw-server/tests", "-v"])
-        if exit_code != 0:
-            print("Tests exit code: " + str(exit_code))
-
-        # Cleanup time
-        sleep(2)
-
-    out, err = server_proc.communicate()
-    out, err = (str(stream, "utf8") for stream in (out, err))
-    print_section(err, title="aw-server output")
+    with _cm_server_process() as server_proc:
+        yield server_proc
 
     error_indicators = ["ERROR"]
-    for s in error_indicators:
-        if s in err:
-            print("Found ERROR indicator in stderr from server: {}".format(s))
-            exit_code = 30 if exit_code == 0 else exit_code
 
-    sys.exit(exit_code)
+    with open(logfile_stdout.name, "r+b") as f:
+        stdout = f.read()
+        if stdout:
+            pytest.fail("Server shouldn't write anything to stdout")
+
+    with open(logfile_stderr.name, "r+b") as f:
+        stderr = str(f.read(), "utf8")
+        if not stderr:
+            pytest.fail("No output to stderr from server")
+
+        # print("STDERR from server:\n" + stderr)
+        for s in error_indicators:
+            if s in stderr:
+                pytest.fail("Found ERROR indicator in stderr from server: {}".format(s))
+
+
+# TODO: Use the fixture in the tests instead of this thing here
+def test_integration(server_process):
+    # This is just here so that the server_process fixture is initialized
+    pass
+
+    # exit_code = pytest.main(["./aw-server/tests", "-v"])
+    # if exit_code != 0:
+    #     pytest.fail("Tests exited with non-zero code: " + str(exit_code))
