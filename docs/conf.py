2681422b6ecb7b455a409387e9dad9e47013d39a
Erik Bjäreholt <erik@bjareho.lt>
2017-08-03 09:57:50 +0200

    increased navigation depth of sidebar ToC

--- docs/conf.py
+++ docs/conf.py
@@ -137,7 +137,10 @@ todo_include_todos = False
 import sphinx_rtd_theme
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_theme_options = {'collapse_navigation': True}
+html_theme_options = {
+    'collapse_navigation': False,
+    'navigation_depth': 4
+}
 using_rtd_theme = True
 
 # The name for this set of Sphinx documents.
