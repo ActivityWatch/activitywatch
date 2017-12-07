f93c403153aba4b11e1b1c6f8a3f83f3b07b9afd
Erik Bjäreholt <erik@bjareho.lt>
2017-12-07 20:42:37 +0100

    updated collect_stats.py

--- scripts/collect_stats.py
+++ scripts/collect_stats.py
@@ -1,29 +1,33 @@
 import re
 from pprint import pprint
+import sys
+from datetime import datetime, timezone
 
 import requests
 
 
-def downloads():
+def downloads(verbose=False):
     r = requests.get("https://api.github.com/repos/ActivityWatch/activitywatch/releases")
     d = r.json()
 
     downloads = 0
     for release in d:
-        print("Release: ", release["tag_name"])
+        if verbose:
+            print("Release: ", release["tag_name"])
         for asset in release["assets"]:
             platform = re.findall("(macos|darwin|linux|windows)", asset["name"])[0]
             count = asset["download_count"]
-            print(" - {}: {}".format(platform, count))
+            if verbose:
+                print(" - {}: {}".format(platform, count))
 
             downloads += asset["download_count"]
-    print("Total: ", downloads)
+    return downloads
 
 
 def stars():
     r = requests.get("https://api.github.com/repos/ActivityWatch/activitywatch")
     d = r.json()
-    print("Stars: ", d["stargazers_count"])
+    return d["stargazers_count"]
 
 
 def clones():
@@ -43,7 +47,11 @@ def twitter():
 
 
 if __name__ == "__main__":
-    stars()
-    downloads()
-    clones()
-    #twitter()
+    s = stars()
+    d = downloads()
+
+    if "--csv" in sys.argv:
+        print("{},{},{}".format(datetime.now(tz=timezone.utc).isoformat(), d, s))
+    else:
+        print("Downloads: ", d)
+        print("Stars:     ", s)
