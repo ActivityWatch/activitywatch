4dbc8d6293df4b190f4c340add972c043e452669
Kerkko Pelttari <kerk.pelt@gmail.com>
2020-03-11 11:29:04 +0200

    Try new long paths solution

--- scripts/ci/enable_long_paths.bat
+++
@@ -1,3 +0,0 @@
-:: Enable long paths on Windows (needed when building since node_modules can create deep hierarchies)
-
-REG ADD "HKLM\SYSTEM\CurrentControlSet\Control\FileSystem" /v LongPathsEnabled /t REG_DWORD /d 1 /f
