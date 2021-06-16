035bd5a0782b845fb16a45e51bcf1f12a9ed7052
Erik Bjäreholt <erik@bjareho.lt>
2021-06-16 14:34:20 +0200

    fix: fixed getversion.sh script used in packaging

--- scripts/package/getversion.sh
+++ scripts/package/getversion.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-set -e
+# set -e
 
 if [[ $TRAVIS_TAG ]]; then
     _version=$TRAVIS_TAG;
 elif [[ $APPVEYOR_REPO_TAG_NAME ]]; then
     _version=$APPVEYOR_REPO_TAG_NAME;
 else
-    _version=$(git rev-parse --short HEAD)
+    # Exact
+    _version=$(git describe --tags --abbrev=0 --exact-match 2>/dev/null)
+    if [[ -z $_version ]]; then
+        # Latest tag + commit ID
+        _version="$(git describe --tags --abbrev=0).dev-$(git rev-parse --short HEAD)"
+    fi
 fi
 
 echo $_version;
