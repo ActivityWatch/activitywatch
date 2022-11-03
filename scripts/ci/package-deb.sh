b6646d42560a62cffff21cc1c69615dd1d90611c
Olivia <zoe.i2k1@gmail.com>
2022-11-03 23:25:39 +0100

    feat: debian packaging

--- scripts/ci/package-deb.sh
+++ scripts/ci/package-deb.sh
@@ -13,3 +13,35 @@ if [ -d "PKGDIR" ]; then
     sudo rm -rf $PKGDIR
 fi
 
+mkdir -p $PKGDIR/DEBIAN
+mkdir -p $PKGDIR/opt
+mkdir -p $PKGDIR/etc/xdg/autostart
+
+# DEBIAN/control metafile. Could make this an explicit file, but 
+# keeping it here opens for shoving VERSION_NUM directly in as a
+# variable.
+# It's a convenience. Nothing more, nothing less.
+debianControlFile="Package: activitywatch
+Architecture: amd64
+Maintainer: Erik Bjäreholt <erik@bjareho.lt>
+Depends:
+Priority: optional
+Version: ${VERSION_NUM}
+Description: Open source time tracker
+ https://github.com/ActivityWatch/activitywatch"
+
+echo $debianControlFile > $PKGDIR/DEBIAN/control
+# The entire opt directory (should) consist of dist/activitywatch/*
+cp -r dist/activitywatch/* $PKGDIR/opt/
+
+# Hard link duplicated libraries
+# (I have no idea what this is for)
+jdupes -L -r -S -Xsize-:1K $PKGDIR/opt/
+
+sudo chown -R root:root $PKGDIR
+
+# Prepare the .desktop file
+sudo sed -i 's!Exec=aw-qt!Exec=/opt/activitywatch/aw-qt!' $PKGDIR/opt/activitywatch/aw-qt.desktop
+sudo cp $PKGDIR/opt/activitywatch/aw-qt.desktop $PKGDIR/etc/xdg/autostart
+
+dpkg-deb --build $PKGDIR
