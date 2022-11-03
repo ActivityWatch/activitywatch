180207bbae9f04bf424cf18b40a188abba423969
Olivia <zoe.i2k1@gmail.com>
2022-11-03 23:13:30 +0100

    fix: spaces are bad in bash variable assignments

--- scripts/ci/package-deb.sh
+++ scripts/ci/package-deb.sh
@@ -1,5 +1,13 @@
 set -e
 
-VERSION = $(scripts/package/getversion.sh)
-echo $VERSION
+VERSION_NUM=$(scripts/package/getversion.sh)
+echo $VERSION_NUM
+PKGDIR="activitywatch_$VERSION_NUM"
+
+# Package tools
+sudo apt install sed jdupes wget
+
+if [ -d "PKGDIR" ]; then
+    sudo rm -rf $PKGDIR
+fi
 
